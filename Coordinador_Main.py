2e33cbe8bf52efacc24792710049375e17537db9
Juan José Restrepo Rosero <49797614+JuanjoRestrepo@users.noreply.github.com>
2023-08-09 20:44:49 -0500

    Archivos Base de Datos que Isa ha creado

---
+++ Coordinador_Main.py
@@ -0,0 +1,77 @@
+import Graph
+import Dashboard
+import Robot_Simulation
+
+
+class coordinator():
+
+    def __init__(self):
+        self.base = Graph.graph()
+        #self.robot = Robot_Simulation.Simulation()
+        self.dash = Dashboard.dashboard()
+
+    def Find_Index_Key(self,data,valor):
+        return(data.index(valor))
+
+
+    # ACCIONES CELDA DE MANUFACTURA
+    def Init_Process(self):
+        dash = self.dash
+        #robot = self.robot
+        base = self.base
+
+        # Bring all the nodes type order with status Created
+        orders_keys,orders_values = base.get_data_especific('order','State','Created')
+        
+        # If we dont have orders to run, notified
+        #if len(orders) == 0:
+        #    return("Lo lamento, en este momento no hay ninguna orden para ejecutar")
+
+        # Run the steps of the order created
+        for order in orders_values:
+
+            material = order[self.Find_Index_Key(orders_keys[0],'Material')]
+            ID = order[self.Find_Index_Key(orders_keys[0],'ID_Order')]
+            
+            # Actualizar información
+            base.update_data(ID,'order','Running','State')
+
+            # Bring the storage information
+            storage = base.get_data('storage')
+            
+        #for i in orders:
+
+    def crear(self,material,amount,piece):
+        dash = self.dash
+        base = self.base
+        keys,values= base.get_data('storage')
+
+		# TRAER UBICACIONES
+        if (material == 'Empack'):
+            pos = 0
+        else:
+            pos = 1
+        
+        disponible = values[0]
+        available = disponible[self.Find_Index_Key(keys[0],'Available')] # valor cantidad disponible material
+        if (int(available[pos]) == 0) or (int(available[pos])< amount):
+            return('Lo lamento no es posible crear la orden por falta de material')
+        
+        else:
+            # CREA LA ORDEN Y ACTUALIZA
+            data = base.create_order(material,amount,piece)
+            dash.Add_End(data,'Ordenes')
+            available[pos] = int(available[pos]) - amount
+            base.update_data('Storage','storage',[available],['Available'])
+            return(available[pos])
+        
+
+
+
+
+run = coordinator()
+"""
+value,v = run.Init_Process()
+print(value,v)
+"""
+run.crear('Aluminio',5,'piece1')
---
+++ Dashboard.py
@@ -0,0 +1,65 @@
+import pygsheets
+import pandas as pd
+
+class dashboard():
+
+    def __init__(self):
+        #authorization
+        self.Path = "OneDrive\Desktop\credentials.json"
+        self.Key = '1FG65WfkqVTcV38nBV5GGBlrH4ZFhSjjzAsvYMbKzCQM'
+        
+
+    def Upload_Sheet_Complete(self,data,SheetName): # Update and change all the sheet information
+        #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
+        gc = pygsheets.authorize(service_file=self.Path)
+        sh = gc.open_by_key(self.Key)
+
+        # Open the Worksheet that you want to update
+        wks = sh.worksheet_by_title(SheetName)
+
+        #update the first sheet with df, starting at cell B2. 
+        wks.clear(start='A2', end=None, fields='userEnteredValue')
+        wks.set_dataframe(data,(2,1))
+
+
+    def Add_End(self,data,SheetName):  # Add a new data in the end
+        #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
+        gc = pygsheets.authorize(service_file=self.Path)
+        sh = gc.open_by_key(self.Key)
+
+        # Open the Worksheet that you want to update
+        wks = sh.worksheet_by_title(SheetName)
+        print(wks.rows)
+        wks.insert_row(data) 
+
+    def Modified_Row(self,ID,data,SheetName,case,col):  # Modificar un dato en específico
+        #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
+        gc = pygsheets.authorize(service_file=self.Path)
+        sh = gc.open_by_key(self.Key)
+
+        # Open the Worksheet that you want to update
+        wks = sh.worksheet_by_title(SheetName)
+
+        cell = wks.find(ID,matchEntireCell=True)
+
+        if case == 1: # Replace one cell
+            wks.update_value((cell[0].row,col),data)
+        
+        else: # Replace all row
+            wks.update_row(cell[0].row,values=data)
+
+    def Delete_Row(self,ID,SheetName): # Delete a specific row
+        #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
+        gc = pygsheets.authorize(service_file=self.Path)
+        sh = gc.open_by_key(self.Key)
+
+        # Open the Worksheet that you want to update
+        wks = sh.worksheet_by_title(SheetName)
+
+        #delete_rows(index, number=1)
+        cell = wks.find(ID,matchEntireCell=True)
+        wks.delete_rows(cell[0].row, number=1)
+
+dash = dashboard()
+data =[1,2,3,4,5,'peep']
+dash.Add_End(data,'Ordenes')
